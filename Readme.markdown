caab33ce08070e3031be955bcdd288f0f7f238c4
zk-phi <zk-phi@users.noreply.github.com>
2020-09-24 00:28:39 +0900

    Initial commit

---
+++ Readme.markdown
@@ -0,0 +1,166 @@
+# alctracker
+
+Manage weekly alcohol consumption from within a Slack channel.
+
+# Setup
+## Install `clasp`
+
+This app can easily be deployed to your Google Drive with `clasp` (an official Google Apps Script API client).
+
+See the repository page (https://github.com/google/clasp) for install instructions.
+
+After installing `clasp`, log-in to your Google account with `clasp login`.
+
+## Deploy the app to your Google Drive
+### Create an empty Google Apps Script with a Spreadsheet
+
+In your Google Drive page, create a new item of the type `Google Spreadsheet` and open.
+
+Then click `Tools > Script Editor` to create an empty Apps Script project associated to the sheet.
+
+Copy the app ID (not sheet ID) in the URL:
+
+```
+https://script.google.com/d/<the-app-id>/edit
+```
+
+FOR SECURITY REASONS, YOU MUST NOT SHARE THE ITEM (especially with Edit permission) WITH OTHERS.
+
+### Deploy the app
+
+Create a file named `.clasp.json` in the root directory of this repository, then register your app's ID as follows:
+
+```
+{"scriptId":"<the-app-id>"}
+```
+
+After setting `scriptId`, you can use `clasp` to deploy the app.
+
+```
+clasp push   # upload the code
+clasp deploy # publish as a webapp
+```
+
+When the app is deployed, a public URL is given to the deployed app.
+
+In the GAS app page, click `Publish` > `Deploy as web app` and you'll see the URL. The URL will be used to connect to Slack later.
+
+## Connect Slack app to the GAS app
+### Create a Slack app
+
+Open the Slack API page (https://api.slack.com/) and click `Your Apps` then `Create New App`.
+
+### Authorize the GAS app to use Slack APIs
+
+In your Slack app page,
+
+- open `OAuth & Permissions` tab
+- in `Scopes` section, click `Add an OAuth Scope` and add `files:write`
+
+  `files:write` is required to upload chart images to Slack.
+
+- from `Tokens for Your Workspace` section, copy the OAuth Access Token to the clipboard
+
+In your GAS app page,
+
+- click `File` > `Project Properties` > `Script Properties` and add a property as follows:
+
+```
+Key: SLACK_ACCESS_TOKEN
+Value: <paste the Slack OAuth Access Token>
+```
+
+### Enable interaction with the GAS app
+
+In your Slack app page,
+
+- open `Interactivity` tab and turn on the feature, then copy-and-paste the GAS app's public URL to the `Request URL` field
+
+- open `Basic Information` tab and copy the `Verification Token` in `App Credentials` section
+
+In your GAS app page,
+
+- click `File` > `Project Properties` > `Script Properties` and add a property as follows:
+
+```
+Key: SLACK_VERIFICATION_TOKEN
+Value: <the Slack Verification Token>
+```
+
+### Add slash command to the Slack app
+
+In your Slack app page,
+
+- open `Slash Commands` tab and turn on the feature, then create a new command and copy-and-paste the GAS app's public URL to the `Request URL` field
+
+### Add an incoming webhook to the Slack app
+
+In your Slack app page,
+
+- open `Incoming Webhooks` tab and turn on the feature, then click `Add New Webhook to Workspace`
+
+In your GAS app page,
+
+- click `File` > `Project Properties` > `Script Properties` and add a property as follows:
+
+```
+Key: SLACK_WEBHOOKURL
+Value: <the Slack webhook URL created in the Slack app page>
+```
+
+```
+Key: SLACK_CHANNEL_NAME
+Value: <the Slack channel name integrated with the app>
+```
+
+In your Slack workspace,
+
+- invite the bot account for the slack app to the channel
+
+## Initialize & authorize the GAS app to use Google APIs
+
+Open `main.gs` in the GAS app page and run `initialize` function manually from the menu `Execute` > `Execute a function` > `intialize`.
+
+You'll be asked to permit the app to use Google Spreadsheet API and outgoing hooks.
+
+After authorization, a message will be posted to your Slack channel.
+
+## Setup triggers
+
+In your GAS app page,
+
+- click `Edit > Current project's trigger` and add a trigger which runs `doTimer` function
+
+  This function creates a spread sheet row to record today's drinks, and posts the summary of the yesterdays' to Slack
+
+My trigger look like this:
+
+```
+function: doTimer
+version: HEAD
+source: time-based
+type: date
+time: <time you usually wake up>
+```
+
+# Development
+
+When you modify the app and want to update the deployment, you can use `-i` option to redeploy without changing the public URL. If `-i` is omitted, you'll have multiple deployments with different versions, in different URLs.
+
+```
+clasp push
+clasp deploy -i <deployment ID>
+```
+
+You can see the list of deployments with `clasp deployments`:
+
+```
+$ clasp deployments
+2 Deployments.
+- <dev deployment ID> @HEAD
+- <deployment ID> @4
+```
+
+The first deployment tagged `@HEAD` is a read-only deployment for dev use, which always runs the latest version of the app. So you'll usually want to redeploy the second deployment.
+
+After creating unneeded deployments, you can also delete the deployment with `clasp undeploy <ID>` command.
---
+++ main.js
@@ -0,0 +1,274 @@
+function initialize () {
+    initializeSheet();
+    postToSlack("Initialization succeeded.");
+}
+
+function _postSummary (title, rowNum, action) {
+    var summary = getSummary(rowNum);
+
+    var blocks = [
+        {
+            type: "section",
+            text: {
+                type: "mrkdwn",
+                text: title
+            }
+        }, {
+            type: "section",
+            text: {
+                type: "mrkdwn",
+                text: "You had " + approx(summary.sum) + " units of alcohol (" + summary.date + ")."
+            }
+        }
+    ].concat(
+        summary.history.map(function (e) {
+            return _formatHistoryItem(e.abv, e.vol, rowNum + "," + e.ix)
+        })
+    );
+
+    if (action) {
+        blocks.push({
+            type: "actions",
+            elements: [{
+                type: "button",
+                text: { type: "plain_text", text: "Add more reports for the day" },
+                action_id: "add",
+                value: rowNum + ""
+            }]
+        });
+    }
+
+    blocks.push({
+        type: "section",
+        text: {
+            type: "mrkdwn",
+            text: "Remaining quota: " + approx(summary.limit - summary.consumed) + " units."
+        }
+    });
+
+    postToSlack("", blocks);
+}
+
+function _formatHistoryItem (abv, vol, actionTarget) {
+    var str = "- ABV: " + abv + "%, VOL: " + vol + "ml = " + approx(abv * vol / 1000) + " in units";
+    var block = {
+        type: "section",
+        text: { type: "mrkdwn", text: str }
+    };
+    if (actionTarget) {
+        block.accessory = {
+            type: "button",
+            text: { type: "plain_text", text: ":pencil2:", emoji: true },
+            action_id: "edit",
+            value: actionTarget
+        };
+    }
+    return block;
+}
+
+function doActionAdd (params) {
+    var strRowNum = params.actions[0].value;
+    var rowNum = Number(params.actions[0].value);
+
+    openSlackModal(params.trigger_id, {
+        type: "modal",
+        title: { type: "plain_text", text: "Add Report" },
+        callback_id: "add",
+        private_metadata: strRowNum,
+        submit: { type: "plain_text", text: "Save" },
+        close: { type: "plain_text", text: "Close" },
+        blocks: [
+            {
+                type: "input",
+                element: {
+                    type: "plain_text_input",
+                    initial_value: "0",
+                    action_id: "abv_value"
+                },
+                label: { type: "plain_text", text: "ABV(%)" },
+                block_id: "abv"
+            }, {
+                type: "input",
+                element: {
+                    type: "plain_text_input",
+                    initial_value: "0",
+                    action_id: "vol_value"
+                },
+                label: { type: "plain_text", text: "Volume(ml)" },
+                block_id: "vol"
+            }
+        ]
+    });
+
+    return ContentService.createTextOutput("");
+}
+
+function doSubmitAdd (params) {
+    var rowNum = Number(params.view.private_metadata);
+
+    var abv = Number(params.view.state.values.abv.abv_value.value);
+    var vol = Number(params.view.state.values.vol.vol_value.value);
+    var item = addToHistory(rowNum, abv, vol);
+
+    _postSummary("Report added!", rowNum, true);
+
+    return ContentService.createTextOutput("");
+}
+
+function doActionEdit (params) {
+    var strId = params.actions[0].value;
+
+    var id = strId.split(",");
+    var value = getHistoryItem(id[0], id[1]);
+
+    openSlackModal(params.trigger_id, {
+        type: "modal",
+        title: { type: "plain_text", text: "Edit Report" },
+        callback_id: "edit",
+        private_metadata: strId,
+        submit: { type: "plain_text", text: "Save" },
+        close: { type: "plain_text", text: "Close" },
+        blocks: [
+            {
+                type: "input",
+                element: {
+                    type: "plain_text_input",
+                    initial_value: value.abv + "",
+                    action_id: "abv_value"
+                },
+                label: { type: "plain_text", text: "ABV(%)" },
+                block_id: "abv"
+            }, {
+                type: "input",
+                element: {
+                    type: "plain_text_input",
+                    initial_value: value.vol + "",
+                    action_id: "vol_value"
+                },
+                label: { type: "plain_text", text: "Volume(ml)" },
+                block_id: "vol"
+            }, {
+                type: "divider"
+            }, {
+                type: "section",
+                text: { type: "mrkdwn", text: "Delete report" },
+                accessory: {
+                    type: "button",
+                    text: { type: "plain_text", text: "Delete" },
+                    style: "danger",
+                    action_id: "delete",
+                    value: strId
+                }
+            }
+        ]
+    });
+
+    return ContentService.createTextOutput("");
+}
+
+function doSubmitEdit (params) {
+    var strId = params.view.private_metadata;
+
+    var abv = Number(params.view.state.values.abv.abv_value.value);
+    var vol = Number(params.view.state.values.vol.vol_value.value);
+    var id = strId.split(",");
+    updateHistory(id[0], id[1], abv, vol);
+
+    _postSummary("Report updated!", id[0]);
+
+    return ContentService.createTextOutput("");
+}
+
+function doActionDelete (params) {
+    var strId = params.actions[0].value;
+
+    var id = strId.split(",");
+    var value = getHistoryItem(id[0], id[1]);
+
+    openSlackModal(params.trigger_id, {
+        type: "modal",
+        callback_id: "delete",
+        private_metadata: strId,
+        title: { type: "plain_text", text: "Delete report" },
+        submit: { type: "plain_text", text: "Delete" },
+        close: { type: "plain_text", text: "Back" },
+        blocks:[
+            {
+                type: "section",
+                text: { type: "mrkdwn", text: "Really delete this report ?" }
+            },
+            _formatHistoryItem(value.abv, value.vol)
+        ]
+    }, true);
+
+    return ContentService.createTextOutput("");
+}
+
+function doSubmitDelete (params) {
+    var strId = params.view.private_metadata;
+
+    var id = strId.split(",");
+    removeFromHistory(id[0], id[1]);
+
+    _postSummary("Deleted the report.", id[0]);
+
+    return ContentService.createTextOutput(JSON.stringify({
+        response_action: "clear"
+    })).setMimeType(ContentService.MimeType.JSON);
+}
+
+function doTimer () {
+    appendRow(new Date());
+    postImageToSlack("Alcohol Tracker summary", getChart(13).getBlob());
+    _postSummary("Alcohol Tracker summary", 3, true);
+}
+
+function doAddLogToday (abv, vol) {
+    var item = addToHistory(2, Number(abv), Number(vol));
+
+    _postSummary("Item added!", 2);
+
+    return ContentService.createTextOutput("");
+}
+
+function doPost (e) {
+    var params = e.parameter.payload ? JSON.parse(e.parameter.payload) : e.parameter;
+
+    var verificationToken = params.token;
+    if (verificationToken != SLACK_VERIFICATION_TOKEN) throw "Invalid token";
+
+    if (params.type) {
+        if (params.type == 'block_actions') {
+            if (params.actions[0].action_id == "edit") {
+                return doActionEdit(params);
+            } else if (params.actions[0].action_id == "delete") {
+                return doActionDelete(params);
+            } else if (params.actions[0].action_id == "add") {
+                return doActionAdd(params);
+            } else {
+                throw "Unknown action";
+            }
+        } else if (params.type == 'view_submission') {
+            if (params.view.callback_id == "edit") {
+                return doSubmitEdit(params);
+            } else if (params.view.callback_id == "delete") {
+                return doSubmitDelete(params);
+            } else if (params.view.callback_id == "add") {
+                return doSubmitAdd(params);
+            } else {
+                throw "Unknown view";
+            }
+        } else {
+            "Unknown action type";
+        }
+    } else { /* slash command */
+        var match = params.text.match(/([0-9.]+) ([0-9.]+)/);
+        if (match) {
+            return doAddLogToday(match[1], match[2]);
+        } else {
+            throw "Invalid params";
+        }
+    }
+
+    throw "Unexpedted error";
+}
